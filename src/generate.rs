497264179d92aafb08359230283717b4bbc0e8ad
Arthur Meyre <arthur.meyre@zama.ai>
2025-03-28 16:19:49 +0100

    feat: add noise squashing data for 1.1

--- src/generate.rs
+++ src/generate.rs
@@ -8,12 +8,13 @@ use bincode::Options;
 use serde::Serialize;
 use tfhe_0_11_versionable::Versionize as VersionizeTfhe_0_11;
 use tfhe_1_0_versionable::Versionize as VersionizeTfhe_1_0;
+use tfhe_1_1_versionable::Versionize as VersionizeTfhe_1_1;
 use tfhe_versionable::Versionize as VersionizeTfhe_0_10;
 use tfhe_versionable::Versionize as VersionizeTfhe_0_8;
 
 use crate::{
     data_dir, dir_for_version, TestCompressionParameterSet, TestDistribution, TestMetadata,
-    TestModulusSwitchNoiseReductionParams, TestParameterSet,
+    TestModulusSwitchNoiseReductionParams, TestNoiseSquashingParams, TestParameterSet,
 };
 
 pub const PRNG_SEED: u128 = 0xdeadbeef;
@@ -110,6 +111,27 @@ pub const INSECURE_SMALL_TEST_PARAMS_MS_NOISE_REDUCTION: TestParameterSet = Test
     }),
 };
 
+/// Those parameters are insecure and are used to generate small legacy public keys
+/// Got with the above parameters for noise squashing
+pub const INSECURE_SMALL_TEST_NOISE_SQUASHING_PARAMS_MS_NOISE_REDUCTION: TestNoiseSquashingParams =
+    TestNoiseSquashingParams {
+        glwe_dimension: 2,
+        polynomial_size: 2048,
+        glwe_noise_distribution: TestDistribution::TUniform { bound_log2: 30 },
+        decomp_base_log: 24,
+        decomp_level_count: 3,
+        modulus_switch_noise_reduction_params: Some(TestModulusSwitchNoiseReductionParams {
+            modulus_switch_zeros_count: 2,
+            ms_bound: 288230376151711744f64,
+            ms_r_sigma_factor: 13.179852282053789f64,
+            ms_input_variance: 2.63039184094559e-7f64,
+        }),
+        message_modulus: 4,
+        carry_modulus: 4,
+        // 0 interpreted as native modulus for u128
+        ciphertext_modulus: 0,
+    };
+
 // Compression parameters for 2_2 TUniform
 pub const VALID_TEST_PARAMS_TUNIFORM_COMPRESSION: TestCompressionParameterSet =
     TestCompressionParameterSet {
@@ -196,6 +218,7 @@ define_store_versioned_test_fn!(store_versioned_test_tfhe_0_8, VersionizeTfhe_0_
 define_store_versioned_test_fn!(store_versioned_test_tfhe_0_10, VersionizeTfhe_0_10);
 define_store_versioned_test_fn!(store_versioned_test_tfhe_0_11, VersionizeTfhe_0_11);
 define_store_versioned_test_fn!(store_versioned_test_tfhe_1_0, VersionizeTfhe_1_0);
+define_store_versioned_test_fn!(store_versioned_test_tfhe_1_1, VersionizeTfhe_1_1);
 
 /// Stores the auxiliary data in `dir`, encoded in cbor, using the right tfhe-versionable version
 macro_rules! define_store_versioned_auxiliary_fn {
