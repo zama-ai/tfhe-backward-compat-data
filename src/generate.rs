9cbe332329df3f4191fe6633004d9536dfd340ac
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2025-06-19 11:18:10 +0200

    chore: add TFHE-rs 1.3 and data for zkv2 fast hash

--- src/generate.rs
+++ src/generate.rs
@@ -9,6 +9,7 @@ use serde::Serialize;
 use tfhe_0_11_versionable::Versionize as VersionizeTfhe_0_11;
 use tfhe_1_0_versionable::Versionize as VersionizeTfhe_1_0;
 use tfhe_1_1_versionable::Versionize as VersionizeTfhe_1_1;
+use tfhe_1_3_versionable::Versionize as VersionizeTfhe_1_3;
 use tfhe_versionable::Versionize as VersionizeTfhe_0_10;
 use tfhe_versionable::Versionize as VersionizeTfhe_0_8;
 
@@ -219,6 +220,7 @@ define_store_versioned_test_fn!(store_versioned_test_tfhe_0_10, VersionizeTfhe_0
 define_store_versioned_test_fn!(store_versioned_test_tfhe_0_11, VersionizeTfhe_0_11);
 define_store_versioned_test_fn!(store_versioned_test_tfhe_1_0, VersionizeTfhe_1_0);
 define_store_versioned_test_fn!(store_versioned_test_tfhe_1_1, VersionizeTfhe_1_1);
+define_store_versioned_test_fn!(store_versioned_test_tfhe_1_3, VersionizeTfhe_1_3);
 
 /// Stores the auxiliary data in `dir`, encoded in cbor, using the right tfhe-versionable version
 macro_rules! define_store_versioned_auxiliary_fn {
@@ -239,6 +241,7 @@ macro_rules! define_store_versioned_auxiliary_fn {
 define_store_versioned_auxiliary_fn!(store_versioned_auxiliary_tfhe_0_8, VersionizeTfhe_0_8);
 define_store_versioned_auxiliary_fn!(store_versioned_auxiliary_tfhe_0_10, VersionizeTfhe_0_10);
 define_store_versioned_auxiliary_fn!(store_versioned_auxiliary_tfhe_0_11, VersionizeTfhe_0_11);
+define_store_versioned_auxiliary_fn!(store_versioned_auxiliary_tfhe_1_3, VersionizeTfhe_1_3);
 
 pub fn store_metadata<Meta: Serialize, P: AsRef<Path>>(value: &Meta, path: P) {
     let serialized = ron::ser::to_string_pretty(value, ron::ser::PrettyConfig::default()).unwrap();
